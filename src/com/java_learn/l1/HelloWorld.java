3460b7c718ff81dbb79ab9c1011952763e1cdd36
Leonid Sielskyi <sielskyi.leonid@gmail.com>
2017-05-03 20:25:27 +0300

    Create HelloWorld.java

---
+++ src/com/java_learn/l1/HelloWorld.java
@@ -0,0 +1,15 @@
+/** 
+*   HelloWorld class
+*
+*/
+
+package com.java_learn;
+
+public class HelloWorld
+{
+    public static void main (String[] argv)
+    {
+        System.out.println("Hello, World!");
+    }
+
+}
